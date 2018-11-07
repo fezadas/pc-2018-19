b49c62c5d3b50d20e0c359a179ee556f6ce6e8d7
fezadas <filipe.machado.fe@gmail.com>
2018-11-07 15:30:16 +0000

    Started exercice 1 series 2

---
+++ NET/PC-SERIE-1/PC-SERIE-2/SafeMessageBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PC_SERIE_2
+{
+    public class SafeMessageBox<M> where M : class
+    {
+        private class MsgHolder
+        {
+            internal M /*readonly*/ msg;
+            internal volatile int lives;
+        }
+
+        private volatile MsgHolder msgHolder = null;
+
+        public void Publish(M m, int lvs)
+        {
+            msgHolder = new MsgHolder { msg = m, lives = lvs };
+        }
+
+        public M TryConsume()
+        {
+            MsgHolder observed, newValue = null;
+            do
+            {
+                observed = msgHolder;
+                if (observed != null && observed.lives > 0) //1
+                {
+                    Interlocked.Decrement(ref observed.lives);
+                    newValue = new MsgHolder { msg = observed.msg, lives = observed.lives };
+                }
+                else return null;
+
+            } while (Interlocked.CompareExchange(ref msgHolder, newValue, observed) != observed);
+            
+            return newValue.msg;
+        }
+    }
+}
